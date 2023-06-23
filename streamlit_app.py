3bf6fa75f1733b61bcde2e32058efc960dd0a23c
piyushitc <106196646+piyushitc@users.noreply.github.com>
2023-06-23 14:42:34 -0500

    updated the menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit as st
 
-st.write('My Parents Diner')
+st.Title('My Parents Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
 
