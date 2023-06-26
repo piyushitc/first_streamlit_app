84c5795574b6065478144938d00e5d4d164f85e3
piyushitc <106196646+piyushitc@users.noreply.github.com>
2023-06-26 14:59:26 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,7 +50,7 @@ except URLError as e:
 
 #Snowflake-related functions
 def get_fruit_load_list():
-  with conn.cursor() as my_cur:
+  with my_cnx.cursor() as my_cur:
     my_cur.execute("select * from fruit_load_list;")
     #conn.execute("select * from fruit_load_list;")
     return my_cur.fetchall()
